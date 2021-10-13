753906301012977f18ea43978306f434d557f644
bisu07 <g26.bishal@gmail.com>
2021-10-12 23:55:55 -0500

    this is initial commit

---
+++ BasicClassExcersie1/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassExcersie1
+{
+    public class Car
+    {
+        public Car() // constructors
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        // properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+    }
+}
---
+++ BasicClassExcersie1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClassExcersie1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "Accord";
+            myCar.Year = 2021;
+
+
+            var nissan = new Car()
+            {
+                Make = "Nissan",
+                Model = "Altima",
+                Year = 2020
+
+            };
+
+
+            var toyota = new Car("Toyota", "Camry", 2022);
+
+            var carList = new List<Car>(){myCar,nissan,toyota};
+            Console.WriteLine(carList.Capacity);
+            Console.WriteLine($"This car dealer have  following cars in stock for this month :");
+
+            foreach (var item in carList)
+            {
+               
+                Console.WriteLine($"{item.Make} {item.Model} {item.Year}");
+            }
+        }
+
+
+    }
+}
